06cd8011fbee374cfd2f800800fd070caecc85d7
nguyen6_phat1 <86005213+phatng45@users.noreply.github.com>
2022-03-02 04:43:17 +0700

    added setting icons, implemented Resources
    Resources.java is used to store all drawable into arrays, though not recommended for using. Will delete if it really is not needed at all

---
+++ app/src/main/java/com/example/moodiary/Resources.java
@@ -0,0 +1,123 @@
+package com.example.moodiary;
+
+public class Resources {
+    protected int[] activityDefault = new int[]{
+            R.drawable.activity_cleaning,
+            R.drawable.activity_cook,
+            R.drawable.activity_date,
+            R.drawable.activity_drawing,
+            R.drawable.activity_eat,
+            R.drawable.activity_family,
+            R.drawable.activity_festival,
+            R.drawable.activity_friend,
+            R.drawable.activity_game,
+            R.drawable.activity_gift,
+            R.drawable.activity_music,
+            R.drawable.activity_party,
+            R.drawable.activity_reading,
+            R.drawable.activity_relax,
+            R.drawable.activity_shopping,
+            R.drawable.activity_sleep,
+            R.drawable.activity_sport,
+            R.drawable.activity_study,
+            R.drawable.activity_swim,
+            R.drawable.activity_tv,
+            R.drawable.activity_walk,
+            R.drawable.activity_work
+    };
+
+    protected int[] activityCustom = new int[]{
+            R.drawable.activity_00,
+            R.drawable.activity_01,
+            R.drawable.activity_02,
+            R.drawable.activity_03,
+            R.drawable.activity_04,
+            R.drawable.activity_05,
+            R.drawable.activity_06,
+            R.drawable.activity_07,
+            R.drawable.activity_08,
+            R.drawable.activity_09,
+            R.drawable.activity_10,
+            R.drawable.activity_11,
+            R.drawable.activity_12,
+            R.drawable.activity_13,
+            R.drawable.activity_14,
+            R.drawable.activity_15,
+            R.drawable.activity_16,
+            R.drawable.activity_17,
+            R.drawable.activity_18,
+            R.drawable.activity_19,
+            R.drawable.activity_20,
+            R.drawable.activity_21,
+            R.drawable.activity_22,
+            R.drawable.activity_23,
+            R.drawable.activity_24,
+            R.drawable.activity_25,
+            R.drawable.activity_26,
+            R.drawable.activity_27,
+            R.drawable.activity_28,
+            R.drawable.activity_29,
+            R.drawable.activity_30,
+            R.drawable.activity_31
+    };
+
+    protected int[] goalDefault = new int[]{
+            R.drawable.goal_deadline,
+            R.drawable.goal_drinking,
+            R.drawable.goal_grow,
+            R.drawable.goal_hobby,
+            R.drawable.goal_instrument,
+            R.drawable.goal_language,
+            R.drawable.goal_learn,
+            R.drawable.goal_read,
+            R.drawable.goal_sleep,
+            R.drawable.goal_study
+    };
+
+    protected int[] moodDefault = new int[]{
+            R.drawable.mood_amazing,
+            R.drawable.mood_awful,
+            R.drawable.mood_happy,
+            R.drawable.mood_ok,
+            R.drawable.mood_sad
+    };
+
+    protected int[] moodCustom = new int[]{
+            R.drawable.mood_00,
+            R.drawable.mood_01,
+            R.drawable.mood_02,
+            R.drawable.mood_03,
+            R.drawable.mood_04,
+            R.drawable.mood_05,
+            R.drawable.mood_06,
+            R.drawable.mood_07,
+            R.drawable.mood_08,
+            R.drawable.mood_09,
+            R.drawable.mood_10,
+            R.drawable.mood_11,
+            R.drawable.mood_12,
+            R.drawable.mood_13,
+            R.drawable.mood_14,
+            R.drawable.mood_15,
+            R.drawable.mood_16,
+            R.drawable.mood_17,
+            R.drawable.mood_18,
+            R.drawable.mood_19,
+            R.drawable.mood_20
+    };
+
+    protected int[] navigation = new int[]{
+            R.drawable.navigation_add,
+            R.drawable.navigation_calendar,
+            R.drawable.navigation_entries,
+            R.drawable.navigation_setting,
+            R.drawable.navigation_stats
+    };
+
+    protected int[] setting = new int[]{
+            R.drawable.setting_account,
+            R.drawable.setting_moods,
+            R.drawable.setting_activities,
+            R.drawable.setting_pin
+    };
+}
