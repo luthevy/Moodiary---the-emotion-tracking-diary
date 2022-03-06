d56cf2087b6c0895cbfdd26b3bc4cadcc176b9c4
nguyen6_phat1 <86005213+phatng45@users.noreply.github.com>
2022-03-06 19:59:09 +0700

    renamed create to edit

--- app/src/main/java/com/example/moodiary/EditEntryActivity.java
+++ app/src/main/java/com/example/moodiary/EditEntryActivity.java
@@ -4,11 +4,11 @@ import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class CreateEntryActivity extends AppCompatActivity {
+public class EditEntryActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_createentry);
+        setContentView(R.layout.activity_editentry);
     }
 }
