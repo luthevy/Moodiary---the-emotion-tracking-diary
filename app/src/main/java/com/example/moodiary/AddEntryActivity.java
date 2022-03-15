5c9607151d5ca8ec9bb23dfe5cec8e15307ddb6e
Mac Hung <machung2001@gmail.com>
2022-03-15 11:47:48 +0700

    Update 2

--- app/src/main/java/com/example/moodiary/AddEntryActivity.java
+++ app/src/main/java/com/example/moodiary/AddEntryActivity.java
@@ -11,6 +11,7 @@ import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
@@ -20,14 +21,25 @@ import androidx.core.widget.ImageViewCompat;
 
 
 public class AddEntryActivity extends AppCompatActivity {
-    private String[] chosenActivities;
+//    private int activityIds[] = {
+//            R.id.activity1,
+//            R.id.activity2,
+//            R.id.activity3,
+//            R.id.activity4,
+//            R.id.activity5,
+//            R.id.activity6,
+//            R.id.activity7,
+//            R.id.activity8,
+//            R.id.activity9};
+    private int chooseStatus[] = {0,0,0,0,0,0,0,0,0};
     private EditText notes;
     private Button btnAddPhoto;
+    private ImageButton btnSave;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_addentry);
+        setContentView(R.layout.activity_main);
 
         LinearLayout activity1 = findViewById(R.id.activity1);
         LinearLayout activity2 = findViewById(R.id.activity2);
@@ -39,23 +51,66 @@ public class AddEntryActivity extends AppCompatActivity {
         LinearLayout activity8 = findViewById(R.id.activity8);
         LinearLayout activity9 = findViewById(R.id.activity9);
         LinearLayout edit_new = findViewById(R.id.edit_new);
+
         notes = findViewById(R.id.edtxt_Notes);
         btnAddPhoto = findViewById(R.id.btnAddPhoto);
+        btnSave = findViewById(R.id.btnSave);
 
-        activity1.setOnTouchListener(new View.OnTouchListener() {
+        activity1.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                if (motionEvent.getAction() == MotionEvent.ACTION_BUTTON_PRESS){
-                    RelativeLayout layout = (RelativeLayout) activity1.getChildAt(0);
-                    ImageView circularShape = (ImageView) layout.getChildAt(0);
-                    ImageView activityImage = (ImageView) layout.getChildAt(1);
-
-                    circularShape.setBackgroundResource(R.drawable.circular_shape);
-                    ImageViewCompat.setImageTintList(activityImage, ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
-                }
-                return true;
+            public void onClick(View view) {
+                clickActivity(activity1,0);
             }
         });
+        activity2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickActivity(activity2,1);
+            }
+        });
+        activity3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickActivity(activity3,2);
+            }
+        });
+        activity4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickActivity(activity4,3);
+            }
+        });
+        activity5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickActivity(activity5,4);
+            }
+        });
+        activity6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickActivity(activity6,5);
+            }
+        });
+        activity7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickActivity(activity7,6);
+            }
+        });
+        activity8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickActivity(activity8,7);
+            }
+        });
+        activity9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                clickActivity(activity9,8);
+            }
+        });
+
         btnAddPhoto.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -77,7 +132,22 @@ public class AddEntryActivity extends AppCompatActivity {
             ImageView imageView = findViewById(R.id.selectedImage);
             imageView.setImageURI(selectedImage);
         }
+    }
 
+    private void clickActivity(LinearLayout activity, int pos){
+        RelativeLayout layout = (RelativeLayout) activity.getChildAt(0);
+        ImageView circularShape = (ImageView) layout.getChildAt(0);
+        ImageView activityImage = (ImageView) layout.getChildAt(1);
+        if (chooseStatus[pos] == 0) {
+            chooseStatus[pos] = 1;
+            circularShape.setImageResource(R.drawable.circular_shape);
+            ImageViewCompat.setImageTintList(activityImage, ColorStateList.valueOf(Color.parseColor("#FFFFFF")));
+        }
+        else if (chooseStatus[pos] == 1) {
+            chooseStatus[pos] = 0;
+            circularShape.setImageResource(R.drawable.circular_shape_none);
+            ImageViewCompat.setImageTintList(activityImage, ColorStateList.valueOf(Color.parseColor("#32CD32")));
+        }
     }
 }
 
