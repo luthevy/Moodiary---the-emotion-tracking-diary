23a2ee5623c337ba514831fa39a7964c8d79621b
nguyen6_phat1 <86005213+phatng45@users.noreply.github.com>
2022-03-30 17:11:01 +0700

    updated choose mood color, animation upon extend list, fixed bugs

--- app/src/main/java/com/example/moodiary/MoodInfor.java
+++ app/src/main/java/com/example/moodiary/MoodInfor.java
@@ -3,18 +3,18 @@ package com.example.moodiary;
 public class MoodInfor {
     public static Integer[][] moods_thumbnail={
             {R.drawable.mood_amazing, R.drawable.mood_02, R.drawable.mood_14},
-            {R.drawable.mood_happy},
+            {R.drawable.mood_happy,R.drawable.mood_00},
             {R.drawable.mood_ok},
             {R.drawable.mood_sad},
-            {R.drawable.mood_awful}
+            {R.drawable.mood_awful,R.drawable.mood_13,R.drawable.mood_03}
     };
 
     public static String [][] moods_type = {
             {"Amazing", "Medium", "Tinker"},
-            {"Happy"},
+            {"Happy","xin"},
             {"Ok"},
             {"Sad"},
-            {"Awful"}
+            {"Awful","Sầu","angry"}
     };
 
     public static String [] moods_color ={"#90DA6E", "#5CEF93", "#45D9FF", "#F5CC67", "#FC6C79"};
