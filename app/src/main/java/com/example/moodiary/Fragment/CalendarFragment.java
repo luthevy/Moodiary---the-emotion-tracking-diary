ea6dc76e1e3d1a368fff5cb68807e9346f7ada56
Lu The Vy <luthevy2001@gmail.com>
2022-04-17 13:52:45 +0700

    Update Calendar View

--- app/src/main/java/com/example/moodiary/Fragment/CalendarFragment.java
+++ app/src/main/java/com/example/moodiary/Fragment/CalendarFragment.java
@@ -24,6 +24,7 @@ import com.prolificinteractive.materialcalendarview.OnMonthChangedListener;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collections;
 import java.util.Date;
 
@@ -33,6 +34,10 @@ public class CalendarFragment extends Fragment {
     private ArrayList<String> listEntry;
     DatabaseReference ref;
     TextView amazing,happy,ok,sad,awful;
+    Calendar c;
+
+    int selectedMonth = 0;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
@@ -43,56 +48,63 @@ public class CalendarFragment extends Fragment {
         ok = v.findViewById(R.id.okStat);
         sad = v.findViewById(R.id.sadStat);
         awful = v.findViewById(R.id.awfulStat);
+        c = Calendar.getInstance();
+        selectedMonth = c.get(Calendar.MONTH) + 1;
+
+        DatabaseReference ref =  FirebaseDatabase.getInstance().getReference("Entry");
+        listEntry=new ArrayList<String>();
+
+        countMoodinMonth(ref, selectedMonth, listEntry);
+
+
         calendarView.setOnMonthChangedListener(new OnMonthChangedListener() {
-            int selectedDate = 0;
             @Override
             public void onMonthChanged(MaterialCalendarView widget, CalendarDay date) {
-                // Do something here
-                selectedDate  = date.getMonth() + 1;
-                DatabaseReference ref =  FirebaseDatabase.getInstance().getReference("Entry");
-//                Get entry from Firebase
-                listEntry=new ArrayList<String>();
-                ref.addValueEventListener(new ValueEventListener() {
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot snapshot) {
-                        listEntry.clear();
-                        for(DataSnapshot dataSnapshot: snapshot.getChildren()){
-                            Entry            entry     = dataSnapshot.getValue(Entry.class);
-                            SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm");
-                            Date date1 = null;
-                            try {
-                                date1 = formatter.parse(entry.getDateOfMood());
-                            } catch (ParseException e) {
-                                e.printStackTrace();
-                            }
-                            int currentMonth = date1.getMonth()+1;
-                            if (currentMonth == selectedDate) {
-                                listEntry.add(entry.getMoodType());
-                            }
-
-                        }
-                        int setAmazingStat = Collections.frequency(listEntry,"Amazing") +
-                                          Collections.frequency(listEntry,"Medium") +
-                                          Collections.frequency(listEntry,"Tinker");
-                        int setHappyStat = Collections.frequency(listEntry,"Happy");
-                        int setAwfulStat = Collections.frequency(listEntry,"angry") +
-                                Collections.frequency(listEntry,"Sầu")+
-                                Collections.frequency(listEntry,"Awful");;
-                        amazing.setText(String.valueOf(setAmazingStat));
-                        happy.setText(String.valueOf(setHappyStat));
-                        ok.setText(String.valueOf(Collections.frequency(listEntry,"Ok")));
-                        sad.setText(String.valueOf(Collections.frequency(listEntry,"Sad")));
-                        awful.setText(String.valueOf(setAwfulStat));
-                    }
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError error) {
-                        System.out.println("ERR read");
-                    }
-                });
-
+                listEntry.clear();
+                selectedMonth  = date.getMonth() + 1;
+                countMoodinMonth(ref, selectedMonth, listEntry);
             }
         });
         return v;
+    }
+    private void countMoodinMonth(DatabaseReference ref, int selectedMonth, ArrayList<String> listEntry){
+        //                Get entry from Firebase
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for(DataSnapshot dataSnapshot: snapshot.getChildren()){
+                    Entry            entry     = dataSnapshot.getValue(Entry.class);
+                    SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+                    Date date1 = null;
+                    try {
+                        date1 = formatter.parse(entry.getDateOfMood());
+                    } catch (ParseException e) {
+                        e.printStackTrace();
+                    }
+                    int currentMonth = date1.getMonth()+1;
+                    if (currentMonth == selectedMonth) {
+                        listEntry.add(entry.getMoodType());
+                    }
+
+                }
+                int setAmazingStat = Collections.frequency(listEntry,"Amazing") +
+                        Collections.frequency(listEntry,"Medium") +
+                        Collections.frequency(listEntry,"Tinker");
+                int setHappyStat = Collections.frequency(listEntry,"Happy");
+                int setAwfulStat = Collections.frequency(listEntry,"angry") +
+                        Collections.frequency(listEntry,"Sầu")+
+                        Collections.frequency(listEntry,"Awful");;
+                amazing.setText(String.valueOf(setAmazingStat));
+                happy.setText(String.valueOf(setHappyStat));
+                ok.setText(String.valueOf(Collections.frequency(listEntry,"Ok")));
+                sad.setText(String.valueOf(Collections.frequency(listEntry,"Sad")));
+                awful.setText(String.valueOf(setAwfulStat));
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                System.out.println("ERR read");
+            }
+        });
     }
 }
 
